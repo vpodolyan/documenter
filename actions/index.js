b3296f82ff7b9bb431e54d64760c6bf876a47031
Vladimir Podolyan <vpodolyan@outlook.com>
2016-11-22 12:21:13 +0300

    Updates

--- actions/index.js
+++
@@ -1,7 +0,0 @@
-export const updateDocument = (fieldName, value) => {
-    return {
-        type: 'UPDATE_DOCUMENT',
-        name: fieldName,
-        value
-    }
-}
--- components/Label.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Label = ({fields, name}) => {
-    return (
-        <span>{fields[name].value}</span>
-    );
-}
-
-export default Label;
